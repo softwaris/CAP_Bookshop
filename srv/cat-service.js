70c777ee3a892d1a61c2c08ef4c5e29a0e60e191
arisvanmazijk <arisvanmazijk@hotmail.com>
2022-09-02 12:07:26 +0200

    srv.on event added

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,9 +1,16 @@
 const cds = require('@sap/cds')
 module.exports = function (){
+  this.on ('READ','Books', async (req,next) => {
+    const books = await next()
+    books.requested = true
+    req.data.test = true
+    console.log(books.data)
+    return books
+  });  
   this.after ('READ','Books', each => {
     if (each.stock > 111) {
       each.title += ` -- 25% discount!`
-      each.req = 'Test Aris'
+      //each.author += ` -- Test Aris`
     }
   });
 }
